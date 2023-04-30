9139ff98fe3e6c5b0a9872ec5b426f8dd47fa6f8
Jose David <80493134+jdalvaradocol@users.noreply.github.com>
2023-04-30 12:47:25 -0500

    D

---
+++ 2023 - I/C/D2/config.h
@@ -0,0 +1,14 @@
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+// Declaracion de constantes
+
+#define SW					GPIO_PORTF_DATA_R		
+
+// Prototipo de las funciones
+
+unsigned long dato_entrada(unsigned long dato);	
+void delay_ms(int delay);	
+
+#endif // __CONFIG_H__
