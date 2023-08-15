27b87990ac524ec9df10c2b482638e2f113742a9
Jose David <80493134+jdalvaradocol@users.noreply.github.com>
2023-08-15 15:36:16 -0500

    Commit

---
+++ 2023II/ASM/E1/main.s
@@ -0,0 +1,19 @@
+
+DATA  	EQU	 	0x20020000
+
+		AREA	|.text|,  CODE, READONLY, ALIGN = 2
+		ENTRY
+		EXPORT	__main
+
+__main
+		LDR 	R1,= DATA     	; Cargar el registro DATA en R0.
+		MOV 	R0,#0x0A        ; R1 = 10
+		STR 	R0,[R1] 		; Guardar el valor de R1 en DATA
+		
+		LDR		R0,= DATA		;0x20000000
+		MOV		R1,#0xBEBE
+		STR		R1,[R0]		
+LOOP		
+		B  		LOOP			
+		ALIGN
+		END			
